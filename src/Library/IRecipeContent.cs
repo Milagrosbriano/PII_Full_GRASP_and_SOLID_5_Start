249c1729d89529774805eee8b5f5589e15cb6659
Milagrosbriano <127800403+Milagrosbriano@users.noreply.github.com>
2023-05-29 20:42:45 -0300

    Create IRecipeContent.cs

---
+++ src/Library/IRecipeContent.cs
@@ -0,0 +1,7 @@
+public interface IRecipeContent
+{
+    string GetTextToPrint();
+}
+
+/*Se creo una interfaz llamada IRecipeContent, la cual cumple con el Grasp isp y dip En resumen,
+ al tener una interfaz con un solo método relevante para su función*/
