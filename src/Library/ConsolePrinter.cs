12dd8c4cebfaa804008d53f8594792270b016d4d
Milagrosbriano <127800403+Milagrosbriano@users.noreply.github.com>
2023-05-29 20:41:22 -0300

    Update ConsolePrinter.cs

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -1,12 +1,16 @@
 using System;
 
+
 namespace Full_GRASP_And_SOLID
 {
+    //Le agregue la interfaz para que la clase ConsolePrinter dependa de esta interfaz, en lugar de depender de la clase Recipe.
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        //Cambie Recipe por los parametros por IRecipeContent para que reciba un objeto de tipo IRecipeContent en lugar de un tipo de la clase Recipe
+        public void PrintRecipe(IRecipeContent recipeContent)
         {
-            Console.WriteLine(recipe.GetTextToPrint());
+            Console.WriteLine(recipeContent.GetTextToPrint());
         }
+
     }
 }
